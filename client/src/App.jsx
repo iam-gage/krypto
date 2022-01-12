9001def671639f362d261fd20958cf3fb65d2e09
gage@gage-webdev-2-box <glezin@gmail.com>
2022-01-12 00:28:16 -0500

    adding screenshot folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { navbar, welcome, footer, services, transactions } from './components';
 
 const App = () =>  {
 
   return (
     <div className="App">
-      <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+    	<div className="main-h-screen">
+		<div className="gradient-bg-welcome">
+			<navbar />
+			<welcome />
+		</div>
+			<services />
+			<transactions />
+			<footer />
+
+	</div>
     </div>
-  )
+  );
 }
 
 export default App
